445a1473c451d01407a1fb8ef23aa31c0f45c9e9
MatTrof <matvey.trofimov13@gmail.com>
2025-01-27 04:39:23 +0300

    ok, 20 changes. this is crazy
    changed a lot of things, added multiple circles, actually working camera and text. this just costed me my fingers!

--- camera_2d.gd
+++ camera_2d.gd
@@ -5,7 +5,9 @@ extends Camera2D
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.y = get_node("/root/Node2D/RigidBody2D").position.y
+	position.y = [get_node("/root/Node2D/White").position.y, get_node("/root/Node2D/Yellow").position.y, get_node("/root/Node2D/Red").position.y,
+		get_node("/root/Node2D/Purple").position.y, get_node("/root/Node2D/Green").position.y, get_node("/root/Node2D/Pink").position.y, 
+		get_node("/root/Node2D/Cyan").position.y].max()
+	
--- label.gd
+++ label.gd
@@ -1,14 +1,27 @@
 extends Label
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var xpos = get_node("/root/Node2D/RigidBody2D").position.x
-	var ypos = get_node("/root/Node2D/RigidBody2D").position.y
-	var rot = get_node("/root/Node2D/RigidBody2D").rotation
-	text = ("Cube X Position: " + var_to_str(xpos) + "\nCube Y Position: " + var_to_str(ypos) + "\nCube Rotation: " + var_to_str(rot * 115))
+	var nodes = [
+		get_node("/root/Node2D/White"),
+		get_node("/root/Node2D/Yellow"),
+		get_node("/root/Node2D/Red"),
+		get_node("/root/Node2D/Purple"),
+		get_node("/root/Node2D/Green"),
+		get_node("/root/Node2D/Pink"),
+		get_node("/root/Node2D/Cyan")
+	]
+	
+	var max_y_position = -INF
+	var max_node_name = ""
+	
+	for node in nodes:
+		if node.position.y > max_y_position:
+			max_y_position = node.position.y
+			max_node_name = node.name
+	
+	text = String(max_node_name + " is in the lead!")
